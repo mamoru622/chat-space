21548c14ae818097fa7d28170c46639ffb128999
mamoru622 <info@takadamamoru.com>
2020-05-04 09:41:22 +0900

    Update user.js
    通信できた場合と通信エラーが起きた場合の処理を実装

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,24 @@
 $(function() {
+  
+  function appendUser(user){
+    let html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+              </div>
+              `;
+    $('#user-search-result').append(html);
+  }
+
+  function appendNoUser(){
+    let html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">ユーザーが見つかりません</p>
+              </div>
+              `;
+    $('#user-search-result').append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
@@ -8,10 +28,20 @@ $(function() {
       dataType: "json"
     })
       .done(function(users) {
-        console.log("成功です");
+        $("#user-search-result").empty();
+        
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            appendUser(user);
+          });
+        } else if (users.length == 0) {
+        return false;
+        } else {
+        appendNoUser();
+        }
       })
       .fail(function() {
-        console.log("失敗です");
+        alert("通信エラーです。ユーザーが表示できません。");
       });
   });
 });
